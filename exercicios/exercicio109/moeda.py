f4adf84b9d94082e5fe5e8e92fca89534c38ca20
Tayssa Antoniasse <tayssaantoniasse123@gmail.com>
2022-06-09 20:56:36 -0300

    ULTIMA MODIFICAÇÃO 109

--- exercicios/exercicio109/moeda.py
+++ exercicios/exercicio109/moeda.py
@@ -1,22 +1,34 @@
 def aumentar(moeda, taxa, formato=False):
     aum = moeda + (moeda*taxa/100)
     #return format(aum)     torna todas as formatações válidas sem escolher se quer ou não formatar
-    return aum if formato is False else moeda(aum)
+    return aum if formato is False else format(aum)
+
 
 def diminuir(moeda, taxa, formato=False):
     dim = moeda - (moeda * taxa/100)
     #return format(dim)
-    return dim if formato is False else moeda(dim)
+    if formato is False:
+        return dim
+    else:
+        return format(dim)
+
 
 def dobro(moeda, formato=False):
     dob = moeda * 2
     #return format(dob)
-    return dob if formato is False else moeda(dob)
+    if not formato:
+        return dob
+    else:
+        return format(dob)
+
 
 def metade(moeda, formato=False):
     met = moeda / 2
     #return format(met)
-    return met if formato is False else moeda(met)
+    if formato == False:
+        return met
+    else:
+        return format(met)
 
 def format(preço = 0, moeda= 'R$'):
     return f'{moeda}{preço:.2f}'.replace('.', ',')
