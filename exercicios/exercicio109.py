3da73f72df283ac7bc0ffa3b00b2f72ef406f6e7
Tayssa Antoniasse <tayssaantoniasse123@gmail.com>
2022-06-09 20:31:25 -0300

    criando a formatação opcional

--- exercicios/exercicio109.py
+++ exercicios/exercicio109.py
@@ -1,7 +1,7 @@
 from exercicio109 import moeda
 
 valor = float(input('Digite o preço: R$').replace(',', '.'))
-print(f'A metade de {moeda.format(valor)} é {moeda.metade(valor)}')
-print(f'O dobro de {moeda.format(valor)} é {moeda.dobro(valor)}')
-print(f'Aumentando 20% de {moeda.format(valor)} temos {moeda.aumentar(valor, 20)}')
-print(f'Reduzindo 25% de {moeda.format(valor)} temos {moeda.diminuir(valor, 25)}')
+print(f'A metade de {moeda.format(valor)} é {moeda.metade(valor, True)}')
+print(f'O dobro de {moeda.format(valor)} é {moeda.dobro(valor, True)}')
+print(f'Aumentando 20% de {moeda.format(valor)} temos {moeda.aumentar(valor, 20, True)}')
+print(f'Reduzindo 25% de {moeda.format(valor)} temos {moeda.diminuir(valor, 25, True)}')
--- exercicios/exercicio109/moeda.py
+++ exercicios/exercicio109/moeda.py
@@ -1,18 +1,22 @@
-def aumentar(moeda, taxa):
+def aumentar(moeda, taxa, formato=False):
     aum = moeda + (moeda*taxa/100)
-    return format(aum)
+    #return format(aum)     torna todas as formatações válidas sem escolher se quer ou não formatar
+    return aum if formato is False else moeda(aum)
 
-def diminuir(moeda, taxa):
+def diminuir(moeda, taxa, formato=False):
     dim = moeda - (moeda * taxa/100)
-    return format(dim)
+    #return format(dim)
+    return dim if formato is False else moeda(dim)
 
-def dobro(moeda):
+def dobro(moeda, formato=False):
     dob = moeda * 2
-    return format(dob)
+    #return format(dob)
+    return dob if formato is False else moeda(dob)
 
-def metade(moeda):
+def metade(moeda, formato=False):
     met = moeda / 2
-    return format(met)
+    #return format(met)
+    return met if formato is False else moeda(met)
 
 def format(preço = 0, moeda= 'R$'):
     return f'{moeda}{preço:.2f}'.replace('.', ',')
