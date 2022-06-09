d9e01b43d82539fff1ea0d55724594a5aca30497
Tayssa Antoniasse <tayssaantoniasse123@gmail.com>
2022-06-09 19:25:02 -0300

    exercicio 108 usando formatação de moedas e pacote

---
+++ exercicios/exercicio108.py
@@ -0,0 +1,7 @@
+from exercicio108 import moeda
+
+valor = float(input('Digite o preço: R$'))
+print(f'A metade de {moeda.moeda(valor)} é {moeda.moeda(moeda.metade(valor))}')
+print(f'O dobro de {moeda.moeda(valor)} é {moeda.moeda(moeda.dobro(valor))}')
+print(f'Aumentando 10% de {moeda.moeda(valor)} temos {moeda.moeda(moeda.aumentar(valor, 10))}')
+print(f'Reduzindo 13% de {moeda.moeda(valor)} temos {moeda.moeda(moeda.diminuir(valor, 13))}')
---
+++ exercicios/exercicio108/moeda.py
@@ -0,0 +1,18 @@
+def aumentar(moeda, taxa):
+    aum = moeda + (moeda*taxa/100)
+    return aum
+
+def diminuir(moeda, taxa):
+    dim = moeda - (moeda * taxa/100)
+    return dim
+
+def dobro(moeda):
+    dob = moeda * 2
+    return dob
+
+def metade(moeda):
+    met = moeda / 2
+    return met
+
+def moeda(preço = 0, moeda= 'R$'):
+    return f'{moeda}{preço:.2f}'.replace('.', ',')
