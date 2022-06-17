6d7a95a93baabe498fa82b41cb43555f10225ec3
Tayssa Antoniasse <tayssaantoniasse123@gmail.com>
2022-06-17 14:19:21 -0300

    exercicio 113 usando tratamento de erros try e except

---
+++ exercicios/exercicio113.py
@@ -0,0 +1,21 @@
+from cores import *
+def leiaInt(txt):
+    try:    
+        n = int(input(txt))
+        return n
+    except ValueError:
+        print(f'{cores["vermelho"]}ERRO! Digite um número inteiro válido.{limpar}')
+        return leiaInt(txt)
+
+
+def leiaReal(txt):
+    try:
+        n = float(input(txt))
+        return n
+    except ValueError:
+        print(f'{cores["vermelho"]}ERRO! Digite um número real válido.{limpar}')
+        return leiaReal(txt)
+
+i = leiaInt('Digite um número INTEIRO: ')
+r = leiaReal('Digite um número REAL: ')
+print(f'Você acabou de digitar os números {i} e {r}!')
