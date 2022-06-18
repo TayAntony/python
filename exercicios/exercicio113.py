80c10b925f5b339faf6ebfceff88c148b93beef5
Tayssa Antoniasse <tayssaantoniasse123@gmail.com>
2022-06-18 16:27:45 -0300

    correção do exercicio 113 usando KeyboardInterrupt

--- exercicios/exercicio113.py
+++ exercicios/exercicio113.py
@@ -1,20 +1,30 @@
 from cores import *
 def leiaInt(txt):
-    try:    
-        n = int(input(txt))
-        return n
-    except ValueError:
-        print(f'{cores["vermelho"]}ERRO! Digite um número inteiro válido.{limpar}')
-        return leiaInt(txt)
+    while True:
+        try:    
+            n = int(input(txt))
+        except (ValueError, TypeError):
+            print(f'{cores["vermelho"]}ERRO! Digite um número inteiro válido.{limpar}')
+            continue
+        except (KeyboardInterrupt):
+            print(f'{cores["vermelho"]}O usuário preferiu não digitar.{limpar}')
+            return 0
+        else:
+            return n
 
+def leiaReal(txt=0):
+    while True:
+        try:
+            n = float(input(txt))
+        except (ValueError, TypeError):
+            print(f'{cores["vermelho"]}ERRO! Digite um número real válido.{limpar}')
+            continue
+        except (KeyboardInterrupt):
+            print(f'{cores["vermelho"]}O usuário preferiu não digitar.{limpar}')
+            return 0
+        else:
+            return n
 
-def leiaReal(txt):
-    try:
-        n = float(input(txt))
-        return n
-    except ValueError:
-        print(f'{cores["vermelho"]}ERRO! Digite um número real válido.{limpar}')
-        return leiaReal(txt)
 
 i = leiaInt('Digite um número INTEIRO: ')
 r = leiaReal('Digite um número REAL: ')
