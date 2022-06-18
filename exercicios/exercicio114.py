c3420b71cfdd8ea59e435f6a83c0e7177b3575dc
Tayssa Antoniasse <tayssaantoniasse123@gmail.com>
2022-06-18 16:36:22 -0300

    correção do ex114 com algumas explicações e uma função read

--- exercicios/exercicio114.py
+++ exercicios/exercicio114.py
@@ -1,10 +1,16 @@
 import urllib
-import urllib.request
+import urllib.request # o request é um módulo interno da urllib
 
 try:
     site=urllib.request.urlopen('http://www.pudim.com.br')
+#urlopen é para tentar abrir a url dentro dos parenteses e aspas
 
 except urllib.request.URLError:
     print('O site PUDIM não está acessível no momento :(')
 else:
     print('O site PUDIM está acessíVel :D')
+    print(site.read())
+    #o código do site (que é bem simples)
+
+# except é para quando dar erro
+# else é para quando da certo
