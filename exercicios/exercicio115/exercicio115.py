eebbb805538ff1388d41146b8cb7226881d6b3ee
Tayssa Antoniasse <tayssaantoniasse123@gmail.com>
2022-06-21 20:54:58 -0300

    ex115 com listagem de cadastros funcional

--- exercicios/exercicio115/exercicio115.py
+++ exercicios/exercicio115/exercicio115.py
@@ -1,26 +1,27 @@
 import inquirer
 from funcoes import *
 
-perguntas = [
-    inquirer.List(
-        'escolha',
-        message = 'MENU',
-        choices = ['Adicionar novo cadastro', 'Logar como ADM', 'Listar cadastros', 'Sair do programa']
-    )
-]
+while True: 
+    perguntas = [
+        inquirer.List(
+            'escolha',
+            message = 'MENU',
+            choices = ['Adicionar novo cadastro', 'Logar como ADM', 'Listar cadastros', 'Sair do programa']
+        )
+    ]
 
-cabecalho('\33[1m\33[34mSISTEMA CADASTRAL    v1.0\33[m')
-respostas = inquirer.prompt(perguntas)
+    cabecalho('\33[1m\33[34m           SISTEMA CADASTRAL    v1.0\33[m')
+    respostas = inquirer.prompt(perguntas)
 
-if respostas['escolha'] == 'Adicionar novo cadastro':
-    novo_cadastro()
+    if respostas['escolha'] == 'Adicionar novo cadastro':
+        novo_cadastro()
 
-elif respostas['escolha'] == 'Logar como ADM':
-    logar_adm()
+    elif respostas['escolha'] == 'Logar como ADM':
+        logar_adm()
 
-elif respostas['escolha'] == 'Listar cadastros':
-    print(3)
+    elif respostas['escolha'] == 'Listar cadastros':
+        listar_cadastros()
 
-elif respostas['escolha'] == 'Sair do programa':
-    sair()
-
+    elif respostas['escolha'] == 'Sair do programa':
+        sair()
+        break
--- exercicios/exercicio115/funcoes.py
+++ exercicios/exercicio115/funcoes.py
@@ -6,7 +6,7 @@ def novo_cadastro():
         print(f'NOME = {nome}\nNASCIMENTO = {nasc}')
         confirmar = input('As informações estão corretas? [S/N] ').upper().strip()
         if confirmar == 'S':
-            print('\33[32m~~~~~~~~~~~~~ CADASTRO EFETUADO COM SUCESSO ~~~~~~~~~~~~~\33[m')
+            print('\33[32m~~~~~~~~~~~ CADASTRO EFETUADO COM SUCESSO ~~~~~~~~~~~\33[m')
             cadastros = open(r'dados_cadastrais.txt', 'a', encoding='utf-8')
             cadastros.write(f'{nome}-{nasc}\n')
             cadastros.close()
@@ -23,18 +23,27 @@ def logar_adm():
         if admin[1] == nasc:
             print('\033[1;35m>>>>>   BEM-VINDA ADM\033[0;0m')
         else:
-            print('\033[1;31mDATA INCORRETA\033[0;0m')
+            print('\033[1;31mDATA INCORRETA, TENTE NOVAMENTE MAIS TARDE!\033[0;0m')
     else:
-        print('\033[1;31mNOME INCORRETO\033[0;0m')
+        print('\033[1;31mNOME INCORRETO, TENTE NOVAMENTE MAIS TARDE!\033[0;0m')
 
 
 def listar_cadastros():
-    pass
+    print('        <<<<<< USUÁRIOS CADASTRADOS >>>>>>')
+    print('-'*50)
+    cadastros = open(r'dados_cadastrais.txt', 'r', encoding='utf-8')
+    dados = cadastros.read()
+    lista_linhas = dados.splitlines()
+    for linha in lista_linhas:
+        l = linha.split('-')
+        print(f'{l[0]:<30} {l[1]:>20}')
+
 
 def sair():
     print('\33[36m\33[4m>>>>>   Até a próxima DEV <3\33[m')
 
+
 def cabecalho(txt):
-    print('-'*60)
-    print(txt.center(60))
-    print('-'*60)
+    print('-'*50)
+    print(txt.center(50))
+    print('-'*50)
